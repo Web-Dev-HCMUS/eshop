0ef7df8c0f10ab60ce618417906ff5a4ef7c76b7
19127618 <91797598+19127618@users.noreply.github.com>
2021-12-20 23:23:38 +0700

    email part2

--- components/auth/authService.js
+++ components/auth/authService.js
@@ -1,6 +1,7 @@
 const userModel = require('../../models/User');
 const bcrypt = require('bcrypt');
 const randomstring = require("randomstring");
+const sgMail = require('../../service/sendGrid');
 
 
 exports.findByUsername = (username) => userModel.findOne({username: username}).lean();
@@ -16,11 +17,45 @@ exports.model = async (user) => {
         return false;
     }
     const passwordHash = await bcrypt.hash(user.password, 10);
-    return userModel.create({
+    await userModel.create({
         username: user.username,
         password: passwordHash,
         email: user.email,
         status: false,
         activationString: randomstring.generate(),
     })
+
+    const msg = {
+        to: user.email, // Change to your recipient
+        from: process.env.EMAIL_SEND, // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      }
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+}
+
+exports.verify_email = (user) => {
+    const msg = {
+        to: user.email, // Change to your recipient
+        from: process.env.EMAIL_SEND, // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      }
+      sgMail
+        .send(msg)
+        .then(() => {
+          console.log('Email sent')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
 }
