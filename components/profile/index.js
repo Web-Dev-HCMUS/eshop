e08be276f424c54fa265f4719bb0c6b25f592f57
19127618 <91797598+19127618@users.noreply.github.com>
2021-12-22 23:55:26 +0700

    change password

--- components/profile/index.js
+++ components/profile/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const aboutControlller = require('./profileController');
+
 router.get('/', aboutControlller.show);
 router.post('/:_id', aboutControlller.update);
 
