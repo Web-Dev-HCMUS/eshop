21006819d2f75ab502013234e69ee292e9f8ea39
Qhope <vuquanghopvn2001@gmail.com>
2022-01-01 21:42:02 +0700

    Update productsController.js

--- components/products/productsController.js
+++ components/products/productsController.js
@@ -1,80 +1,119 @@
-const Product = require('../../models/Product');
+const Product = require("../../models/Product");
 const mongooseObject = require("../../ulti/mongoose");
 
 // change limit product will show on products page
 const perPage = 9;
 
 class productsController {
-    // [GET] /
-    async index(req, res, next) {
-        const totalDoc = await Product.find({}).count();
-        const totalPage = Math.ceil(totalDoc / perPage);
-        const currentPage = req.query.page || 1;
-        const products = await Product.find({})
-            .skip(perPage * (currentPage - 1))
-            .limit(perPage);
+  // [GET] /
+  async index(req, res, next) {
+    const totalDoc = await Product.find({}).count();
+    const totalPage = Math.ceil(totalDoc / perPage);
+    const currentPage = req.query.page || 1;
+    const products = await Product.find({})
+      .skip(perPage * (currentPage - 1))
+      .limit(perPage);
 
-        res.render("../components/products/views/products", {
-            products: mongooseObject.multipleMongooseToObject(products),
-            totalPage: totalPage,
-            user: req.user,
-        });
-    }
+    res.render("../components/products/views/products", {
+      products: mongooseObject.multipleMongooseToObject(products),
+      totalPage: totalPage,
+      user: req.user,
+    });
+  }
 
-    // [GET] /:type
-    async show(req, res, next) {
-        if (req.params.type === "search") {
-            console.log("params", req.params.type);
-            let filter = {};
-            const keyword = req.query.name;
-            if (keyword !== "") {
-                filter = {
-                    name: {
-                        $regex: keyword,
-                        $options: "i",
-                    },
-                };
-            }
-            const sortParams = req.query.sort;
-            const orderParams = req.query.order;
-            let sort = { updatedAt: "-1" };
-            if (sortParams === "price") {
-                if (orderParams === "asc") {
-                    sort = { price: "1" };
-                } else {
-                    sort = { price: "-1" };
-                }
-            }
+  // [GET] /:type
+  async show(req, res, next) {
+    if (req.params.type === "search") {
+      console.log("params", req.params.type);
+      let filter = {};
+      const keyword = req.query.name;
+      if (keyword !== "") {
+        filter = {
+          name: {
+            $regex: keyword,
+            $options: "i",
+          },
+        };
+      }
+      const category = req.query.category;
+      if (category !== "default" && category !== "0") {
+        filter.type = category;
+      }
+      const rangePrice = req.query.rangePrice;
 
-            const totalDoc = await Product.find(filter).count();
-            const totalPage = Math.ceil(totalDoc / perPage);
-            const currentPage = req.query.page || 1;
-
-            const products = await Product.find(filter)
-                .sort(sort)
-                .collation({ locale: "en_US", numericOrdering: true })
-                .skip(perPage * (currentPage - 1))
-                .limit(perPage);
-
-            res.render("../components/products/views/products", {
-                products: mongooseObject.multipleMongooseToObject(products),
-                totalPage: totalPage,
-            });
-        } else {
-            const totalDoc = await Product.find({ type: req.params.type }).count();
-            const totalPage = Math.ceil(totalDoc / perPage);
-            const currentPage = req.query.page || 1;
-            const products = await Product.find({ type: req.params.type })
-                .skip(perPage * (currentPage - 1))
-                .limit(perPage);
-
-            res.render("../components/products/views/products", {
-                products: mongooseObject.multipleMongooseToObject(products),
-                totalPage: totalPage,
-                user: req.user
-            });
+      if (rangePrice !== "0") {
+        switch (rangePrice) {
+          case "1":
+            filter.price = {
+              $lte: 10000000,
+            };
+            break;
+          case "2":
+            filter.price = {
+              $gte: 10000000,
+              $lte: 25000000,
+            };
+            break;
+          case "3":
+            filter.price = {
+              $gte: 25000000,
+            };
+            break;
+          default:
+            filter.price = {
+              $gte: 1,
+            };
+            break;
         }
+      }
+
+      const release = req.query.release;
+      const sortParams = req.query.sort;
+      const orderParams = req.query.order;
+      let sort = {};
+      if (release === "0") {
+        sort = { updatedAt: "-1" };
+      } else {
+        sort = { updatedAt: release };
+      }
+
+      if (sortParams === "price") {
+        if (orderParams === "asc") {
+          sort = { price: "1" };
+        } else {
+          sort = { price: "-1" };
+        }
+      }
+
+      const totalDoc = await Product.find(filter).count();
+      const totalPage = Math.ceil(totalDoc / perPage);
+      const currentPage = req.query.page || 1;
+
+      const products = await Product.find(filter)
+        .sort(sort)
+        .collation({ locale: "en_US", numericOrdering: true })
+        .skip(perPage * (currentPage - 1))
+        .limit(perPage);
+
+      res.render("../components/products/views/products", {
+        products: mongooseObject.multipleMongooseToObject(products),
+        totalPage: totalPage,
+      });
+    } else {
+      const totalDoc = await Product.find({ type: req.params.type }).count();
+      const totalPage = Math.ceil(totalDoc / perPage);
+      const currentPage = req.query.page || 1;
+      const products = await Product.find({ type: req.params.type })
+        .skip(perPage * (currentPage - 1))
+        .limit(perPage);
+
+      res.render("../components/products/views/products", {
+        products: mongooseObject.multipleMongooseToObject(products),
+        totalPage: totalPage,
+        user: req.user,
+      });
     }
+  }
 }
 
 module.exports = new productsController();
